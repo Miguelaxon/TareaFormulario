f8d319224f9e2db28c7e37ea6888bc1914a2f3ee
miguel <miguel_ahumada_c@hotmail.com>
2020-12-17 13:46:05 -0300

    se configura el View Binding

--- app/src/main/java/com/example/tareaformulario/MainActivity.java
+++ app/src/main/java/com/example/tareaformulario/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.tareaformulario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.tareaformulario.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String eLog = MainActivity.class.getSimpleName();
+    private ActivityMainBinding eBinding;
+    private String nombre, apellido, correo, clave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        Log.d(eLog,"APP Iniciada");
+
+        eBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(eBinding.getRoot());
+
+        eBinding.btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (eBinding.textView.getText().toString().isEmpty()) {
+                    Log.d(eLog, "Nombre vacío");
+                } else {
+                    nombre = eBinding.textView.getText().toString();
+                }
+
+            }
+        });
     }
 }
